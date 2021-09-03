f21479763e42c76b1df127418653b22ae248adb7
Aybek Rahmatillaev <aibek.kdm90@gmail.com>
2021-09-03 15:40:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Sign from "./Components/Sign";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [user, setUser] = useState(null);
+    if(user === null){
+           return <Sign setUser={(u)=>setUser(u)}/>
+    }
+    return (
+        <div >
+           <h1>Welcome</h1>
+        </div>
+    )
+
 }
-
-export default App;
---
+++ src/Components/Sign.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import {Form, InputGroup, FormControl, Button, Row, Col} from "react-bootstrap";
+import axios from "axios";
+import {useState} from "react";
+import {ToastContainer,toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+function Sign(props) {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [status, setStatus] = useState('');
+    const [loading, setLoading] = useState(0);
+    const [bul, setBul] = useState(false);
+
+    const error = () => {
+        toast.error('Код не совпадают', {
+            position: "bottom-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme:"colored"
+        });
+    };
+
+
+
+    const send = ()=> {
+        setBul(true)
+        const a = axios.post(`https://api.chat.besoft.kg/v1/auth`, {
+            phone_number: 996 + phoneNumber
+        })
+        a.then(({data}) => {
+            console.log(data)
+            if (data.status.startsWith("not_exists")) {
+                setLoading(1)
+                setStatus('register')
+            }else if(data.status.startsWith("exists_code")){
+                setLoading(2)
+                setStatus('loading')
+            }
+        })
+        a.catch((err) => {
+            console.log(err)
+        })
+        a.finally(() => {
+            setBul(false)
+        })
+    }
+        const signIn = ()=>{
+            setBul(true)
+            const a = axios.post(`https://api.chat.besoft.kg/v1/auth/register`,{
+                phone_number:996 + phoneNumber,
+                verification_code:password,
+                platform:'web',
+                version_code:1,
+                full_name:fullName,
+            })
+
+            a.then(({data}) => {
+                    console.log(data)
+                if(data.status === 'success'){
+                    alert('welcome')
+                    props.setUser(data.payload.user)
+                }else{
+                    error()
+                }
+            })
+            a.catch((err)=>{
+                console.log(err)
+            })
+            a.finally(()=>{
+                setBul(false)
+            })
+            }
+            const signUp = () => {
+                setBul(true)
+                const a = axios.post(`https://api.chat.besoft.kg/v1/auth/login`,{
+                    phone_number:996 + phoneNumber,
+                    verification_code:password,
+                    platform:'web',
+                    version_code:1
+                })
+                a.then(({data})=>{
+                    console.log(data)
+                    if(data.status === 'success'){
+                        props.setUser(data.payload.user)
+                    }else{
+                        error()
+                    }
+                })
+                a.catch((err)=>{
+                    console.log(err)
+                })
+                a.finally(()=>{
+                    setBul(false)
+                })
+            }
+
+
+
+    return (
+        <>
+            <Row className={'d-flex justify-content-center'}>
+                <Col xm={8} md={6}>
+                    <ToastContainer/>
+                    <div className={'container mt-5  border p-5 '}>
+                        <h2>BESOFT OFFICE</h2>
+                        <Form>
+
+                            <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
+                                Username
+                            </Form.Label>
+                            <InputGroup className="mb-2 mb-4">
+                                <InputGroup.Text>+996</InputGroup.Text>
+                                <FormControl
+                                    disabled={loading}
+                                    type={'number'}
+                                    onChange={(e)=>setPhoneNumber(e.target.value)}
+                                    id="inlineFormInputGroup" placeholder="Номер телефона"/>
+                            </InputGroup>
+                            {status !== ''&& status === 'register' ?
+                                <FormControl
+                                    className={'mb-4'}
+                                    onChange={(e)=>setFullName(e.target.value)}
+                                    id="inlineFormInputGroup" placeholder="Имя"/>:null}
+                            {status !== ''?
+                                <FormControl
+                                    type={'number'}
+                                    className={'mb-4'}
+                                    onChange={(e)=>setPassword(e.target.value)}
+                                    id="inlineFormInputGroup" placeholder="Код подверждение"/>:null}
+                            <Button
+                                disabled={
+                                    phoneNumber.length !==9 ||bul||
+                                    (status === 'register' && (password.length !== 4 || fullName.length<1)
+                                    )}
+                                onClick={()=>{
+                                    if(loading === 0){
+                                        send()
+                                    }else if (loading === 1){
+                                        signIn()
+                                    }else if(loading === 2){
+                                        signUp()
+                                    }
+                                }}
+                                className={'w-100 mb-3'}>{status === ''? 'Отправить код':'Подтвердить'}</Button>
+                        </Form>
+
+                    </div>
+                </Col>
+            </Row>
+
+        </>
+    );
+}
+
+export default Sign;
